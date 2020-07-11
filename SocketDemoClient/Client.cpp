74f5701306e6f5ef88d912c5b0812570c515817a
ttthoaic6.pbchau <ttthoaic6.pbchau@gmail.com>
2020-07-11 09:59:48 +0700

    fix code client

--- SocketDemoClient/Client.cpp
+++ SocketDemoClient/Client.cpp
@@ -16,56 +16,61 @@
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
+
 #include <csignal>
 #include <iostream>
 #define bzero(b,len) (memset((b), '\0', (len)), (void) 0)
 
 using namespace std;
 
-// The one and only application object
-
 int sockfd = 0;
 char name[32];
 #define LENGTH 2048
 volatile sig_atomic_t flag = 0;
 
-
-void str_overwrite_stdout() {
-	printf("\r%s", "> ");
+void str_overwrite_stdout() 
+{
+	printf("%s", "> ");
 	fflush(stdout);
 }
 
 void str_trim_lf(char* arr, int length) 
 {
-	for (int i = 0; i < length; i++) { // trim \n
-		if (arr[i] == '\n') {
+	for (int i = 0; i < length; i++) 
+	{ 
+		if (arr[i] == '\n') 
+		{
 			arr[i] = '\0';
 			break;
 		}
 	}
 }
 
-void catch_ctrl_c_and_exit(int sig) {
+void catch_ctrl_c_and_exit(int sig) 
+{
 	flag = 1;
 }
 
-void* send_msg_handler(void* arg) {
+void* send_msg_handler(void* arg) 
+{
 	char message[LENGTH] = {};
 	char buffer[LENGTH + 32] = {};
 
-	while (1) {
+	while (true) 
+	{
 		str_overwrite_stdout();
 		fgets(message, LENGTH, stdin);
 		str_trim_lf(message, LENGTH);
 
-		if (strcmp(message, "exit") == 0) {
+		if (strcmp(message, "exit") == 0) 
+		{
 			break;
 		}
-		else {
-			sprintf(buffer, "%s: %s\n", name, message);
+		else 
+		{
+			sprintf(buffer, "%s: %s", name, message);
 			send(sockfd, buffer, strlen(buffer), 0);
 		}
-
 		bzero(message, LENGTH);
 		bzero(buffer, LENGTH + 32);
 	}
@@ -74,23 +79,23 @@ void* send_msg_handler(void* arg) {
 	return NULL;
 }
 
-void* recv_msg_handler(void * arg) {
+void* recv_msg_handler(void * arg) 
+{
 	char message[LENGTH] = {};
-	while (1) {
+	while (true) 
+	{
 		int receive = recv(sockfd, message, LENGTH, 0);
-		if (receive > 0) {
-			printf("%s", message);
+		if (receive > 0) 
+		{
+			printf("%s\n", message);
 			str_overwrite_stdout();
 		}
-		else if (receive == 0) {
+		else if (receive == 0) 
+		{
 			break;
-		}
-		else {
-			// -1
 		}
 		memset(message, 0, sizeof(message));
 	}
-
 	return NULL;
 }
 
@@ -106,12 +111,14 @@ int main()
 		printf("Failed. Error Code : %d", WSAGetLastError());
 		return 1;
 	}
+	printf("Connect to Server successfull!!!\n");
 
 	cout << "Please enter your name: ";
 	fgets(name, 32, stdin);
 	str_trim_lf(name, strlen(name));
 
-	if (strlen(name) > 32 || strlen(name) < 2) {
+	if (strlen(name) > 32 || strlen(name) < 2) 
+	{
 		printf("Name must be less than 30 and more than 2 characters.\n");
 		return EXIT_FAILURE;
 	}
@@ -125,33 +132,35 @@ int main()
 
 	int err = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 	if (err == -1) {
-		printf("ERROR: connect\n");
+		printf("ERROR: Not connect to Server\n");
 		return EXIT_FAILURE;
 	}
 
 	send(sockfd, name, 32, 0);
-
 	cout << "=== WELCOME TO THE CHATROOM ===" << endl;
 
 	pthread_t send_msg_thread;
-	if (pthread_create(&send_msg_thread, NULL, &send_msg_handler, NULL) != 0) {
+	if (pthread_create(&send_msg_thread, NULL, &send_msg_handler, NULL) != 0) 
+	{
 		printf("ERROR: pthread\n");
 		return EXIT_FAILURE;
 	}
 
 	pthread_t recv_msg_thread;
-	if (pthread_create(&recv_msg_thread, NULL, &recv_msg_handler, NULL) != 0) {
+	if (pthread_create(&recv_msg_thread, NULL, &recv_msg_handler, NULL) != 0) 
+	{
 		printf("ERROR: pthread\n");
 		return EXIT_FAILURE;
 	}
 
-	while (1) {
-		if (flag) {
+	while (true) 
+	{
+		if (flag) 
+		{
 			printf("\nBye\n");
 			break;
 		}
 	}
-
 	closesocket(sockfd);
 
 	return EXIT_SUCCESS;
